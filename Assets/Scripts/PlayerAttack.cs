ba44f1cfc207b11866aa88059dbd4a5aa23a7232
gamulchi <32320763+gamulchi@users.noreply.github.com>
2019-09-19 14:31:56 +0900

    에러 없는 것 확인

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public GameObject Bullet;
+    public Text CText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!CText)
+        {
+            CText = GameObject.FindGameObjectWithTag("Canvas").transform.Find("Text").gameObject.GetComponent<Text>();
+        }   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) {
+            CText.text = "MOUSE TOUCH";
+        }
+        if (Input.touchCount>0)
+
+        {
+            CText.text = "SCREEn TOUCH";
+        }
+        if (Bullet)
+        {
+            //Instantiate(Bullet, transform.parent);
+        }
+    }
+}
